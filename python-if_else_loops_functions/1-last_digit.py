deb3e0e235dd5a84aa9f0426d7b862d987b1bbc8
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-01-13 02:17:41 +0400

    Fixing result issue

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ result = f"Last digit of {number} is {last_digit} and"
 if last_digit > 5:
     result += " is greater than 5"
 elif last_digit < 6 and last_digit != 0:
-    result += " and is less than 6 and not 0"
+    result += " is less than 6 and not 0"
 else:
     result += " is 0"
 print(result)
