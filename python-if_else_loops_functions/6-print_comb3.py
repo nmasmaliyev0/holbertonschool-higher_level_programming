70664db1d3d1551dbb8cc0ef445db4c86050735c
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-01-13 04:51:33 +0400

    Shorten the code on line 4

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}".format(int(str(i) + str(j))), end=", " if i != 8 else "\n")
+        print("{:02d}".format(10*i+j), end=", " if i != 8 else "\n")
