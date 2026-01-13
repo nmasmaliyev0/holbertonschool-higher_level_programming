d06adfa1ca0d2af723974fed1e714d76fa05bf7e
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-01-13 05:11:38 +0400

    Add Task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        if 97 <= ord(char) <= 122:  #if the char is lowercase:
+            result += chr( ord(char) - 32 )
+        else:
+            result += char
+    print("{}".format(result))
