b6831d443e360996971af3d00825dbc4b34405c5
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-01-17 01:46:12 +0400

    Add Task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def main():
+    if len(argv) - 1 == 0:
+        print("{} arguments.".format(0))
+        return
+    elif len(argv) - 1 == 1:
+        print("{} argument:".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+
+    for i in range(1, len(argv) - 1):
+        print("{}: arguments".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    main()
