5b0c06f5987733cd1f6740e50b313dd5deafcc1f
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-01-17 06:29:06 +0400

    Make some changes on the code

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new_list = my_list
+
     if idx < 0 or idx >= len(my_list):
-        return None
+        return new_list
 
-    new_list = []
-
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
-        else:
-            new_list.append(element)
+    new_list[idx] = element
 
     return new_list
