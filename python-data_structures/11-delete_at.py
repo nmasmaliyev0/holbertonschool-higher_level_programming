eacdf33630f945d70290d5766ae4fa4a87b400c5
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-01-18 01:47:24 +0400

    Make a change on the file

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,6 +3,6 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
     
-    my_list = [my_list[i] for i in range(len(my_list)) if i != idx]
+    del my_list[idx]
 
     return my_list
