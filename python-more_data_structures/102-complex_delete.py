8ea8e39767ac89edcce711d2bd00ae8b9b4aa5b1
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-01-18 06:43:17 +0400

    Make some changes on the file

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new_dict = {k: v for k, v in a_dictionary.items() if v != value}
+    keys = {k: v for k, v in a_dictionary.items() if v == value}
 
-    return new_dict
+    for key in keys:
+        del a_dictionary[key]
+
+    return a_dictionary
