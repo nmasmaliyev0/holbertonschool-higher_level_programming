e9c7b753b591f27bda021190796917788087793c
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-01-18 05:34:48 +0400

    Make a change

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
+    if len(a_dictionary) == 0 or a_dictionary == None:
         return None
     
     max_value = max(a_dictionary.values())
