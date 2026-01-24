4480f2f0929dda8ea677f825e3efd523e9171805
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-01-25 03:52:34 +0400

    Add Task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
+    for element in my_list:
+        if count == x:
+            break
+
+        try:
+            print("{:d}".format(element), end="")
+            count += 1
+
+        except (ValueError, TypeError):
+            continue
+
+    print()
+    return count
