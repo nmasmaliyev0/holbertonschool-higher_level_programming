b32f7e3d86de44f364bea4b4525495bbe3fabf08
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-01-25 05:32:29 +0400

    Delete unnecessary whitespace

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,7 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             value = 0
-        
+
         except IndexError:
             print("out of range")
             value = 0
