68353610de3b0e4c069b6cd783e50cda514e59b5
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-01-26 06:35:15 +0400

    Add Task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module introduces basic Python class structure, private attributes."""
+
+
+class Square:
+    """This class represents a square."""
+    def __init__(self, size=0):
+        """Initialiing a Square instance object with a given size"""
+        self.__size = size
+
+    def size(self):
+        """Using Getter for retrieving size"""
+        return self.__size
+
+    def size(self, size):
+        """Using Setter to accept value for the __size by validation"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Creates a public instance method that return area of the square"""
+        return self.__size ** 2
