27dc87b38ceca0b40b3f46b3f1cf9aa1a1744b08
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-02-17 23:27:18 +0400

    Add Task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+"""This module provides basic understanding of json strings"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """This function turns obj into json string and writes it to a file"""
+    with open(filename, "w") as file:
+        json_str = json.dumps(my_obj)
+
+        file.write(json_str)
