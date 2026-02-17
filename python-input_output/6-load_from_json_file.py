7b208804d90a90d68265b304ed7ada1946ffa85e
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-02-17 23:35:57 +0400

    Shorten the docstring

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -4,7 +4,7 @@ import json
 
 
 def load_from_json_file(filename):
-    """This function reads a json string inside of a file and turns it to and object"""
+    """This function reads a json string and turns it to and object"""
     with open(filename) as file:
         json_str = file.read()
         json_obj = json.loads(json_str)
