2a1d61af23320c4046edeb07f39b76b4dfc9716a
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-02-19 14:31:03 +0400

    Add module docstring

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This module provides a simple way to turn class instances into dictionaries"""
+
+
 def class_to_json(obj):
     """This function turns a class instance into a dictionary"""
     return obj.__dict__
