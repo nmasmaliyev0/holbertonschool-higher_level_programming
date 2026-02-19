028a19463d2d6494bfde4ee39303ca081ce1de0b
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-02-19 15:52:21 +0400

    Add Task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module shows a way to turn class instances to dicts"""
+
+
+class Student:
+    """This class defines a new Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dict represantation of Student based on keys"""
+        if isinstance(attrs, list):
+            if all(isinstance(attr, str) for attr in attrs):
+                new_dict = {}
+
+                for attr in attrs:
+                    if attr in self.__dict__:
+                        new_dict[attr] = self.__dict__[attr]
+
+        return self.__dict__
+    
+    def reload_from_json(self, json):
+        """Replace attributes using dictionry"""
+        for key, value in json.items():
+            setattr(self, key, value)
