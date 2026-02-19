9121d805e1ca0aff41dd092cbc50b750d51a526b
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-02-19 15:53:01 +0400

    Remove extra whitespace

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -21,7 +21,7 @@ class Student:
                         new_dict[attr] = self.__dict__[attr]
 
         return self.__dict__
-    
+
     def reload_from_json(self, json):
         """Replace attributes using dictionry"""
         for key, value in json.items():
