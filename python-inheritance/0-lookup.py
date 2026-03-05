380f20466b3008a01b23b8a7510a241e7c4d35bf
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-03-05 15:54:29 +0400

    Add Task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module explains how to show attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Returns available attributes and methods"""
+    return dir(obj)
