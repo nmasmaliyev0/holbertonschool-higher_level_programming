39b6201a2a6cf50f34f100ed0206cfbe5df522f0
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-03-05 16:45:44 +0400

    Fixing typo

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,4 +14,4 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
 
         if value <= 0:
-            raise ValueError("{} must be greater than 0".fornat(name))
+            raise ValueError("{} must be greater than 0".format(name))
