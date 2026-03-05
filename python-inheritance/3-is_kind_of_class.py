4df5f8e085e5ccd4895940158f319d4cf450c3cc
nmasmaliyev0 <nicatmesmeliyev0@gmail.com>
2026-03-05 16:23:17 +0400

    Add Task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Checks if object is an instance of a class."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if obj is instance of a_class"""
+    return isinstance(obj, a_class)
